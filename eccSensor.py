a7cb21229fd83d930608ef58f1a3d162ef9c5eb9
mariapazGomez <81169168+mariapazGomez@users.noreply.github.com>
2022-12-12 23:06:33 -0300

    Create eccSensor.py

---
+++ eccSensor.py
@@ -0,0 +1,94 @@
+from tinyec import registry
+from Crypto.Cipher import AES
+import hashlib, secrets, binascii
+import socket
+import threading
+import sys
+import pickle
+
+def encrypt_AES_GCM(msg, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+def ecc_point_to_256_bit_key(point):
+    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
+    sha.update(int.to_bytes(point.y, 32, 'big'))
+    return sha.digest()
+
+curve = registry.get_curve('brainpoolP256r1')
+
+def encrypt_ECC(msg, pubKey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    sharedECCKey = ciphertextPrivKey * pubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    ciphertext, nonce, authTag = encrypt_AES_GCM(msg, secretKey)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    return (ciphertext, nonce, authTag, ciphertextPubKey)
+
+def decrypt_ECC(encryptedMsg, privKey):
+    (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
+    sharedECCKey = privKey * ciphertextPubKey
+    secretKey = ecc_point_to_256_bit_key(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
+
+#Wait for incoming data from server
+#.decode is used to turn the message in bytes to a string
+def receive(socket, signal):
+    while signal:
+        try:
+            data = socket.recv(32)
+            print(str(data.decode("utf-8")))
+        except:
+            print("You have been disconnected from the server")
+            signal = False
+            break
+
+
+msg = b'Text to be encrypted by ECC public key and ' \
+      b'decrypted by its corresponding ECC private key'
+print("original msg:", msg)
+privKey = secrets.randbelow(curve.field.n)
+pubKey = privKey * curve.g
+
+encryptedMsg = encrypt_ECC(msg, pubKey)
+encryptedMsgObj = {
+    'ciphertext': binascii.hexlify(encryptedMsg[0]),
+    'nonce': binascii.hexlify(encryptedMsg[1]),
+    'authTag': binascii.hexlify(encryptedMsg[2]),
+    'ciphertextPubKey': hex(encryptedMsg[3].x) + hex(encryptedMsg[3].y % 2)[2:]
+}
+print("encrypted msg:", encryptedMsgObj)
+
+decryptedMsg = decrypt_ECC(encryptedMsg, privKey)
+print("decrypted msg:", decryptedMsg)
+
+#Get host and port
+host = '192.168.1.107'
+port = 3333
+
+#Attempt connection to server
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+except:
+    print("Could not make a connection to the server")
+    input("Press enter to quit")
+    sys.exit(0)
+
+#Create new thread to wait for data
+receiveThread = threading.Thread(target = receive, args = (sock, True))
+receiveThread.start()
+msg = pickle.dumps(encryptedMsgObj)
+
+#Send data to server
+#str.encode is used to turn the string message into bytes so it can be sent across the network
+while True:
+     msg = bytes(f'{len(msg):<{10}}',"utf-8") + msg
+     sock.send(msg)
